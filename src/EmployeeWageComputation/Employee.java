04acbcadfa3fef36a8da58229393cfc8b8d2598e
AkshatDahikar <akshatdahikar@gmail.com>
2024-11-26 23:14:10 +0530

    Added:Calculating Wages for a Month

--- src/EmployeeWageComputation/Employee.java
+++ src/EmployeeWageComputation/Employee.java
@@ -6,30 +6,35 @@ public class Employee {
 
 	public static void main(String[] args) {
 		System.out.println("Welcome to Employee Wage Computation Program on master branch");
-		int Wage_Per_Hours=20;
-		int Full_Day_Hours=8;
-		int Part_Time_Hours=4;
+		final int Wage_Per_Hours=20;
+		final int Full_Day_Hours=8;
+		final int Part_Time_Hours=4;
+		final int Working_Days_In_Month=20; 
 		Random r1=new Random();
+		int totalMonthlyWage=0;
+		for(int day=1 ;day <= Working_Days_In_Month;day++) {
 		int attendance=r1.nextInt(3);
 	    int dailyWage=0;
 	    switch(attendance) {
 	    case 0:
-	    	System.out.println("Employee is Absent");
+	    	System.out.println("Day"+day+": Employee is Absent");
 	    	dailyWage=0;
 	    	break;
 	    case 1:
-	    	System.out.println("Employee is working part-time ");
+	    	System.out.println("Day"+day+": Employee is working part-time ");
 	    	dailyWage =Wage_Per_Hours*Part_Time_Hours;
 	    	break;
 	    case 2: 
-	    	System.out.println("Employee is working full-time");
+	    	System.out.println("Day"+day+"Employee is working full-time");
+	    	dailyWage =Wage_Per_Hours*Full_Day_Hours;
 	    	break;
 	    	default:
 	    		
 	    		System.out.println("Invalid Attendance ");
 	    }
-	    
-	    System.out.println("Daily Employee Wage using switch case: "+ dailyWage);
+	    totalMonthlyWage =totalMonthlyWage+dailyWage;
+		}
+	    System.out.println("Total Monthly Employee Wage : "+ totalMonthlyWage);
 	    
 		}
 	}
