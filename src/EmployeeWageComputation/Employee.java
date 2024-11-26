7ce7c22d662cd03477a8145ee5bc16e831c246b8
AkshatDahikar <akshatdahikar@gmail.com>
2024-11-26 15:43:46 +0530

    Welcome to Employee Wage Computation

---
+++ src/EmployeeWageComputation/Employee.java
@@ -0,0 +1,9 @@
+package EmployeeWageComputation;
+
+public class Employee {
+
+	public static void main(String[] args) {
+		System.out.println("Welcome to Employee Wage Computation Program on master branch");
+	}
+
+}
