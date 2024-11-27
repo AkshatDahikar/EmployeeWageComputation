5e2153eb8f2969240cbdf92b76f0ec05000098a9
AkshatDahikar <akshatdahikar@gmail.com>
2024-11-27 18:24:57 +0530

    Added : Calculate Daily Employee Wages

--- src/EmployeeWageComputation2/Employee.java
+++ src/EmployeeWageComputation2/Employee.java
@@ -5,12 +5,17 @@ import java.util.Random;
 public class Employee {
 public static void main(String[] args) {
 	System.out.println("Welcome to Employee Wage Computation Program on Master Branch");
+	int Wage_Per_hours = 20;
+	int Full_Day_Hours=8;
 	Random r1=new Random();
 	int attendance=r1.nextInt(2);
 	if(attendance == 1) {
 		System.out.println("Employee is present ");
+		int dailyEmployeeWage = Wage_Per_hours * Full_Day_Hours;
+		System.out.println("Daily Employee Wage:"+dailyEmployeeWage);
 	}else {
 		System.out.println("Employee is absent ");
+		System.out.println("Daily Employee Wage: 0");
 	}
 	}
 }
