26b0c8aefd025a7d1ee55fcadf484006c58d7802
AkshatDahikar <akshatdahikar@gmail.com>
2024-11-27 10:35:51 +0530

    Added : Calculate Wages

--- src/EmployeeWageComputation/Employee.java
+++ src/EmployeeWageComputation/Employee.java
@@ -6,30 +6,47 @@ public class Employee {
 
 	public static void main(String[] args) {
 		System.out.println("Welcome to Employee Wage Computation Program on master branch");
-		int Wage_Per_Hours=20;
-		int Full_Day_Hours=8;
-		int Part_Time_Hours=4;
+		final int Wage_Per_Hours=20;
+		final int Full_Day_Hours=8;
+		final int Part_Time_Hours=4;
+		final int Max_Working_Hours=100;
+		final int Max_Working_Days=10; 
 		Random r1=new Random();
+		int totalWorkingHours =0;
+		int totalWorkingDays=0;
+		int totalMonthlyWage=0;
+		while(totalWorkingHours < Max_Working_Hours && totalWorkingDays < Max_Working_Days) {
+			totalWorkingDays++;
 		int attendance=r1.nextInt(3);
-	    int dailyWage=0;
+	    int dailyHours=0;
 	    switch(attendance) {
 	    case 0:
-	    	System.out.println("Employee is Absent");
-	    	dailyWage=0;
+	    	System.out.println("Day"+totalWorkingDays+": Employee is Absent");
+	    	dailyHours=0;
 	    	break;
 	    case 1:
-	    	System.out.println("Employee is working part-time ");
-	    	dailyWage =Wage_Per_Hours*Part_Time_Hours;
+	    	System.out.println("Day"+totalWorkingDays+": Employee is working part-time ");
+	    	dailyHours =Part_Time_Hours;
 	    	break;
 	    case 2: 
-	    	System.out.println("Employee is working full-time");
+	    	System.out.println("Day"+totalWorkingDays+"Employee is working full-time");
+	    	dailyHours =Full_Day_Hours;
 	    	break;
 	    	default:
 	    		
-	    		System.out.println("Invalid Attendance ");
+	    		System.out.println("Invalid Attendance Type");
 	    }
-	    
-	    System.out.println("Daily Employee Wage using switch case: "+ dailyWage);
+	    totalWorkingHours =totalWorkingHours+dailyHours;
+	    int dailyWage=Wage_Per_Hours * dailyHours;
+	    totalMonthlyWage =totalMonthlyWage + dailyWage;
+	    if(totalWorkingHours >= Max_Working_Hours) {
+	    	System.out.println("Maximum working hours limit reached");
+	    	break;
+	    }
+		}
+		System.out.println("Total Working Days: "+totalWorkingDays);
+		System.out.println("Total Working Hours: "+totalWorkingHours);
+	    System.out.println("Total Monthly  Wage : "+ totalMonthlyWage);
 	    
 		}
 	}
