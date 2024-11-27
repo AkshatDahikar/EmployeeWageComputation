4db3a22464fdbc35b5d0cb0bafa82bdc5bfe5682
AkshatDahikar <akshatdahikar@gmail.com>
2024-11-27 18:16:16 +0530

    Added : Check Attendence2

--- src/EmployeeWageComputation2/Employee.java
+++ src/EmployeeWageComputation2/Employee.java
@@ -8,9 +8,9 @@ public static void main(String[] args) {
 	Random r1=new Random();
 	int attendance=r1.nextInt(2);
 	if(attendance == 1) {
-		System.out.println("Employee is present");
+		System.out.println("Employee is present ");
 	}else {
-		System.out.println("Employee is absent");
+		System.out.println("Employee is absent ");
 	}
 	}
 }
