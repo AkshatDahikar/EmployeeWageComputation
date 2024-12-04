57a580ea4dbea29f742ca136a679365da242e335
AkshatDahikar <akshatdahikar@gmail.com>
2024-12-04 23:23:29 +0530

    Calculate Wage

--- src/EmployeeWageComputation/EmployeeAttendance.java
+++ src/EmployeeWageComputation/EmployeeAttendance.java
@@ -3,13 +3,9 @@ package EmployeeWageComputation;
 import java.util.Random;
 
 public class EmployeeAttendance {
-	 public void printAttendance() {
-	        Random random = new Random();
-	        int attendance = random.nextInt(2);
-	         if (attendance == 0) {
-	             System.out.println(" Employee is absent");
-	         }else {
-	             System.out.println(" Employee is present");
+	public int getAttendance() {
+        Random random = new Random();
+        return random.nextInt(2);
 	         }
 	    }
-}
+
