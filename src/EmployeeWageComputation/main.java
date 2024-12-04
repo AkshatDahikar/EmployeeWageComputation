6409b381c310d266ad2e3ac8e8fc095c368d70c6
AkshatDahikar <akshatdahikar@gmail.com>
2024-12-04 23:48:03 +0530

    Added : Calculate Wages

--- src/EmployeeWageComputation/main.java
+++ src/EmployeeWageComputation/main.java
@@ -3,9 +3,10 @@ package EmployeeWageComputation;
 public class main {
 
 	public static void main(String[] args) {
+	    System.out.println("Welcome to Employee Wage Computation");
 		  EmployeeAttendance employeeAttendance = new EmployeeAttendance();
 	        CalculateEmployeeWage calculateEmployeeWage = new CalculateEmployeeWage();
-	                int attendance=employeeAttendance.getAttendance();
+	                int attendance = employeeAttendance.getAttendance(); 
 	              calculateEmployeeWage.calculateEmployeeWage(attendance);
 	}
 
