19fee43ecf5a73d8f6ec6a1595fa9d520e4cd8cd
AkshatDahikar <akshatdahikar@gmail.com>
2024-12-05 08:56:43 +0530

    PArtTime Employee

--- src/EmployeeWageComputation/CalculateEmployeeWage.java
+++ src/EmployeeWageComputation/CalculateEmployeeWage.java
@@ -1,19 +1,24 @@
 package EmployeeWageComputation;
 
 public class CalculateEmployeeWage {
-	  private static final int Wage_Per_Hrs=20;
-	    private static final int Full_Day_Hrs=8;
-	    public void calculateEmployeeWage(int attendance) {
-	        int workingHrs=0;
 
-	        if(attendance==1){
-	            System.out.println("Employee is present");
-	            workingHrs=Full_Day_Hrs;
-	        }else{
-	            System.out.println("Employee is absent");
-	        }
+    private static final int Wage_Per_Hrs=20;
+    private static final int Full_Day_Hrs=8;
+    private static final int Part_Time_Hrs=8;
+    public int calculateEmployeeWage(int attendance) {
+        int workingHrs=0;
 
-	        int dailyWage=workingHrs*Wage_Per_Hrs;
-	        System.out.println("Employee wage: "+dailyWage);
-	    }
+        if(attendance==1){
+            System.out.println("Employee is present");
+            workingHrs=Full_Day_Hrs;
+        }else if(attendance==2){
+            System.out.println("Employee doing part time ");
+            workingHrs=Part_Time_Hrs;
+        }else{
+            System.out.println("Employee is absent");
+        }
+
+        return workingHrs*Wage_Per_Hrs;
+
+    }
 }
