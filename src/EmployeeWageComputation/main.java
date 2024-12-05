e2a62ad393efe572e1b7c4e6ef34e3a9ee5868a5
AkshatDahikar <akshatdahikar@gmail.com>
2024-12-05 09:06:12 +0530

    Added : Switch Case

--- src/EmployeeWageComputation/main.java
+++ src/EmployeeWageComputation/main.java
@@ -2,12 +2,21 @@ package EmployeeWageComputation;
 
 public class main {
 
+	private static final int Working_Days_Per_Month = 20;
+
 	public static void main(String[] args) {
-	    System.out.println("Welcome to Employee Wage Computation");
+	    System.out.println(" Welcome to Employee Wage Computation ");
 		  EmployeeAttendance employeeAttendance = new EmployeeAttendance();
 	        CalculateEmployeeWage calculateEmployeeWage = new CalculateEmployeeWage();
-	                int attendance = employeeAttendance.getAttendance(); 
-	              calculateEmployeeWage.calculateEmployeeWage(attendance);
+	        int monthlyWage=0;
+	        for(int day=1;day<= Working_Days_Per_Month;day++) {
+	            int attendance = employeeAttendance.getAttendance();
+	            int dailyWage=calculateEmployeeWage.calculateEmployeeWage(attendance);
+	            monthlyWage+=dailyWage;
+	            System.out.println("Daily wage " +dailyWage);
+	        }
+	        System.out.println("Total wage "+monthlyWage);
+
+	    }
 	}
 
-}
